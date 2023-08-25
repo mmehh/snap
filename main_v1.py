44dbfab9dea9c97f4e752399453d47d67272f52d
meh <mihalceamarian82@gmail.com>
2023-08-25 12:30:18 +0300

    renamed files

--- main_v1.py
+++ main_v1.py
@@ -4,24 +4,20 @@ import asyncio
 import time
 from playwright.async_api import async_playwright
 import helpers.file as f
-import helpers.browser as b
 
 
 async def main():
-    """Main function using asyncio tasks and browser new context
+    """Main function
     """
     start = time.perf_counter()
     async with async_playwright() as play:
         browser = await play.chromium.launch()
-
+        page = await browser.new_page()
         url_list = f.get_urls()
-        tasks = set()
-        async with asyncio.TaskGroup() as task_group:
-            for url in url_list:
-                tasks.add(task_group.create_task(b.take_shot(
-                    browser, url, f.dir_path() + '/' + f.file_name(url))))
+        for url in url_list:
+            await page.goto(url)
+            await page.screenshot(path=f.dir_path() + '/' + f.file_name(url), full_page=True)
         await browser.close()
     end = time.perf_counter()
     print(f'It took {round(end-start,0)} second(s) to complete.')
-
 asyncio.run(main())
